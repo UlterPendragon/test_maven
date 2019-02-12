2fa28aa9710378b2843778aed34853d32ecc90e0
Ulter <brendansiche@gmail.com>
2019-02-12 15:49:56 +0100

    commit tp 3 maven

--- src/main/java/dev/App.java
+++ src/main/java/dev/App.java
@@ -1,16 +1,22 @@
 package dev;
 
+import java.util.ResourceBundle;
 import com.github.lalyos.jfiglet.FigletFont;
 
 /**
  * Hello world! lolol
  *
  */
-public class App 
-{ 
-    public static void main( String[] args )
-    {
-    	 System.out.println(FigletFont.convertOneLine( "Hello World!  I  rock !" ));
-    	System.out.println(FigletFont.class.getResourceAsStream("/flf/font2.flf"));
-    }
-    }
+public class App { 
+
+	public static void main( String[] args)  {
+		String titre = ResourceBundle.getBundle("application").getString("titre");
+		String asciiArt = FigletFont.convertOneLine(titre); 
+		System.out.println(asciiArt);
+		// System.out.println(FigletFont.convertOneLine( "Hello World!  I  rock !" ));
+
+		//affichage de l'environnement
+		String environnement = ResourceBundle.getBundle("application").getString("environnement");
+		System.out.println("Environnement : " + environnement);
+	}
+}
